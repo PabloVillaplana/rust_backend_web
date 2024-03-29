5e8ca771b5858979bbee05b273fba46481b96cb5
Juan Pablo Villaplana Corrales <jpvillaplana@gmail.com>
2024-03-28 22:56:54 -0600

    Add Bootstrap and some styles

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,15 @@ extern crate diesel;
 pub mod models;
 pub mod schema; // Modelos
 
-use actix_web::post;
+// use actix_web::post;
 use actix_web::web;
 use diesel::prelude::*;
 use diesel::r2d2;
 use diesel::r2d2::ConnectionManager;
 use platzi_rust_blog_post::establish_pool_connection;
+use tera::Tera;
 
-use self::models::{NewPost, Post};
+use self::models::Post;
 use self::schema::posts::dsl::*;
 
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
@@ -19,7 +20,7 @@ use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 pub type DbPool = r2d2::Pool<ConnectionManager<MysqlConnection>>;
 
 #[get("/")]
-async fn index(pool: web::Data<DbPool>) -> impl Responder {
+async fn index(pool: web::Data<DbPool>, template_manager: web::Data<tera::Tera>) -> impl Responder {
     match pool.get() {
         Ok(conn) => {
             match web::block(move || {
@@ -31,12 +32,16 @@ async fn index(pool: web::Data<DbPool>) -> impl Responder {
                 Ok(data) => {
                     let mut respuesta = String::new();
 
-                    for post in data.unwrap() {
+                    let data = data.unwrap();
+                    for post in &data {
                         let new_post = format!("{}\n{}\n", post.title, post.body);
                         respuesta.push_str(&new_post);
                     }
 
-                    HttpResponse::Ok().body(respuesta)
+                    let mut ctx = tera::Context::new();
+                    ctx.insert("posts", &data);
+
+                    HttpResponse::Ok().body(template_manager.render("index.html", &ctx).unwrap())
                 }
                 Err(_) => HttpResponse::InternalServerError().finish(),
             }
@@ -45,31 +50,23 @@ async fn index(pool: web::Data<DbPool>) -> impl Responder {
     }
 }
 
-#[post("/new_post")]
-async fn add_post(pool: web::Data<DbPool>) -> impl Responder {
-    match pool.get() {
-        Ok(conn) => {
-            match web::block(move || {
-                let mut conn = conn;
-
-                let new_post = NewPost {
-                    title: "Nuevo Post",
-                    body: "Cuerpo del post",
-                    slug: "slug",
-                };
-                diesel::insert_into(posts)
-                    .values(&new_post)
-                    .execute(&mut conn)
-            })
-            .await
-            {
-                Ok(_) => HttpResponse::Ok().finish(),
-                Err(_) => HttpResponse::InternalServerError().finish(),
-            }
-        }
-        Err(_) => HttpResponse::InternalServerError().finish(),
-    }
-}
+// #[post("/new_post")]
+// async fn add_post(pool: web::Data<DbPool>, item: web::Json<NewPostHandler>) -> impl Responder {
+//     match pool.get() {
+//         Ok(conn) => {
+//             match web::block(move || {
+//                 let mut conn = conn;
+//                 Post::create_post(&mut conn, &item)
+//             })
+//             .await
+//             {
+//                 Ok(_) => HttpResponse::Ok().finish(),
+//                 Err(_) => HttpResponse::InternalServerError().finish(),
+//             }
+//         }
+//         Err(_) => HttpResponse::InternalServerError().finish(),
+//     }
+// }
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -78,9 +75,12 @@ async fn main() -> std::io::Result<()> {
     let pool_connection = establish_pool_connection();
 
     HttpServer::new(move || {
+        let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*")).unwrap();
+
         App::new()
             .service(index)
-            .service(add_post)
+            // .service(add_post)
+            .app_data(web::Data::new(tera))
             .app_data(web::Data::new(pool_connection.clone()))
     }) // move es para mover ownership de pool_connection a la closure de HttpServer el hilo principal
     .bind(("127.0.0.1", 8080))?
--- src/models.rs
+++ src/models.rs
@@ -8,6 +8,8 @@ pub struct Post {
     pub slug: String,
 }
 
+// use crate::schema::posts::{slug, title};
+
 use super::schema::posts;
 
 #[derive(Insertable)]
@@ -16,4 +18,56 @@ pub struct NewPost<'a> {
     pub title: &'a str,
     pub body: &'a str,
     pub slug: &'a str,
+}
+
+// Serializador
+#[derive(Serialize, Deserialize, Debug)]
+pub struct NewPostHandler {
+    pub title: String,
+    pub body: String,
+    pub slug: String,
+}
+
+//Funciones Implementadas
+
+// use diesel::mysql::MysqlConnection;
+// use diesel::{connection, prelude::*};
+
+impl Post {
+    pub fn slugify(other_title: &String) -> String {
+        return other_title
+            .to_lowercase()
+            .replace(" ", "-")
+            .replace("?", "")
+            .replace("¿", "")
+            .replace("¡", "")
+            .replace("!", "")
+            .replace(":", "")
+            .replace(";", "")
+            .replace(",", "")
+            .replace(".", "")
+            .replace("á", "a")
+            .replace("é", "e")
+            .replace("í", "i")
+            .replace("ó", "o")
+            .replace("ú", "u")
+            .replace("ñ", "n");
+    }
+
+    // pub fn create_post<'a>(
+    //     connection: &mut MysqlConnection,
+    //     post: &NewPostHandler,
+    // ) -> Result<Post, diesel::result::Error> {
+    //     let other_slug = Post::slugify(&post.title);
+
+    //     let new_post = NewPost {
+    //         title: &post.title.as_str(),
+    //         body: &post.body.as_str(),
+    //         slug: &other_slug,
+    //     };
+
+    //     return diesel::insert_into(posts::table)
+    //         .values(new_post)
+    //         .get_result::<Post>(connection);
+    // }
 }
